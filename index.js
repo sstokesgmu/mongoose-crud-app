870ae5982911a208c5ff37f02fa924a7cf9b1069
sstokes <sterlingjstokes@gmail.com>
2024-12-19 10:29:24 -0500

    Setup up the express server

---
+++ index.js
@@ -0,0 +1,9 @@
+//Start server
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port: ${process.env.PORT}`);
+})
